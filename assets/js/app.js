04ef71c35e6591f53e98ca02b1b241cce5940079
supernora00 <yourshing00@gmail.com>
2026-07-17 23:36:42 +0800

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
-  document.querySelectorAll('[data-check]').forEach((el)=>{
-    const key='nora-v1-'+location.pathname+'-'+el.dataset.check;
-    el.checked=localStorage.getItem(key)==='1';
-    el.addEventListener('change',()=>localStorage.setItem(key,el.checked?'1':'0'));
-  });
-});
